a55eae5a0283722fc6d1e14de83e4fc573f82a68
student <parvathy_p@hcl.com>
2021-04-20 16:06:28 +0000

    Initialcommit

---
+++ DevopsGraded/src/main/java/com/hcl/graded/DevopsGraded/DevopsGradedApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.graded.DevopsGraded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
